2bb79660ca80223ba410abb4ff303d2a2d930a28
‘h‘fhfhf52hf526 <x13155916315@qq.com>
2019-05-15 00:45:05 +0800

    Signed-off-by: ‘h‘fhfhf52hf526 <x13155916315@qq.com>

--- app/apis/v1/logintoken.py
+++ app/apis/v1/logintoken.py
@@ -1,21 +1,10 @@
+"""生成token"""
 from flask_restful import Resource
 from app.models import User
 from app.forms.tokenfrom import *
 from app.config.status_code import *
 from flask import jsonify
-from app.libs.token import create_auto_token
-
-
-class GetToken(Resource):
-    def post(self):
-        request_data = get_login()
-        username = User.query.filter_by(username=request_data[Loginfrom.username]).first()
-        if username:
-            password = request_data[Loginfrom.password]
-            if User.check_pwd(username, password):
-                token = create_auto_token(username.username, username.role)
-                return jsonify({"token": token})
-        return UserNull
+from app.libs.token import Verifytoken
 
 
 class Login(Resource):
--- app/libs/token.py
+++ app/libs/token.py
@@ -1,15 +1,14 @@
+"""
+生成token的方法,用户名+权限+当前时间
+并且验证token传递name合法性
+"""
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
 import time
 from app.config.status_code import *
 from app.config.config import SECRET_KEY
-
-"""
-生成token，用户名+权限+当前时间
-"""
-
-"""生成token的方法"""
-
+from collections import namedtuple
+from flask import g
 
 def create_auto_token(username, Permission, nowtime=time.time()):
     expiration = int(time.time()) + 7200 - int(nowtime)
@@ -18,10 +17,17 @@ def create_auto_token(username, Permission, nowtime=time.time()):
     return str(token, 'utf-8')
 
 
-"""token装饰器"""
-
-
 def Verifytoken(token):
+    UserInfo = Gettokenmessage(token)
+    if UserInfo:
+        g.user = UserInfo
+        print(g.user)
+        return True
+    else:
+        return False
+
+
+def Gettokenmessage(token):
     s = Serializer(SECRET_KEY, expires_in=7200)
     try:
         data = s.loads(token)
@@ -31,6 +37,7 @@ def Verifytoken(token):
         return TokenExpired
     username = data['username']
     Permission = data['Permission']
+    User = namedtuple('Token', ['username', 'Permission'])
+    return User(username='username', Permission='Permission')
 
-
-create_auto_token(1, 1)
+create_auto_token("测试",'1')
